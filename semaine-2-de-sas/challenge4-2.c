65ca62a6e9edae73abdc3df7ea25001e6d614a2b
adnankhayrou <adnanekhayrou3@gmail.com>
2022-09-14 15:00:45 +0100

    challenge 4-2

--- semaine-2-de-sas/challenge4-2.c
+++ semaine-2-de-sas/challenge4-2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+int x;
 bool isPremier(int a, int b){
-    int x = a/b;
+     x = a/b;
     int s=0;
     for (int i = 1; i <= x; i++)
     {
@@ -21,10 +21,16 @@ int main(){
 int a,b;
 printf("entree nomber N1 : ");
 scanf("%d",&a);
+ther :
 printf("entree nomber N2: ");
 scanf("%d",&b);
+if (b <= 0)
+{
+    goto ther;
+}
+
 if (isPremier(a,b)==true){
-    printf("le nomber est premie.");
+    printf("le nomber %d est premie.", x);
 }else
- printf("le nomber n premie.");
+ printf("le nomber %d non premie.", x);
 }
